2d3eb519d52a4ae5604f6a04b581f04f3c88a916
Andre Vanha <andre@vanhas.com>
2026-02-27 10:31:15 -0700

    Implemented skeleton of HE TunnelBroker plugin.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/avanha/pmaas-plugin-hetunnelbroker
+
+go 1.25.7
---
+++ plugin.go
@@ -0,0 +1,39 @@
+package hetunnelbroker
+
+import (
+	"github.com/avanha/pmaas-plugin-hetunnelbroker/config"
+	spi "github.com/avanha/pmaas-spi"
+)
+
+type plugin struct {
+	pluginConfig config.PluginConfig
+	container    spi.IPMAASContainer
+}
+
+func NewPluginConfig() config.PluginConfig {
+	return config.PluginConfig{}
+}
+
+type Plugin interface {
+	spi.IPMAASPlugin
+}
+
+func NewPlugin(pluginConfig config.PluginConfig) Plugin {
+	instance := &plugin{
+		pluginConfig: pluginConfig,
+	}
+
+	return instance
+}
+
+func (p *plugin) Init(container spi.IPMAASContainer) {
+	p.container = container
+}
+
+func (p *plugin) Start() {
+
+}
+
+func (p *plugin) Stop() chan func() {
+	return p.container.ClosedCallbackChannel()
+}
